b3b61bdf349b7a451211aedfb1010713936e86e2
Kayla Galway <kayla.galway@gmail.com>
2020-05-28 15:13:15 -0600

    add board struct and implementation functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,90 @@
 use std::io;
 
+struct Board {
+    array: [&'static str; 9],
+}
+
+impl Board {
+    fn print_board(&self) {
+        // Board should print as:
+        // |0|1|2|
+        // |3|4|5|
+        // |6|7|8|
+        println!("
+        |{}|{}|{}|
+        |{}|{}|{}|
+        |{}|{}|{}|",
+        self.array[0],
+        self.array[1],
+        self.array[2],
+        self.array[3],
+        self.array[4],
+        self.array[5],
+        self.array[6],
+        self.array[7],
+        self.array[8]
+        );
+    }
+
+    // Compare characters by indexes on board to verify if there is currently a winner
+    fn check_for_win(&self) -> bool {
+        if self.array[0] == self.array[1] && self.array[1] == self.array[2] {
+            return true;
+        } else if self.array[3] == self.array[4] && self.array[4] == self.array[5] {
+            return true;
+        } else if self.array[6] == self.array[7] && self.array[7] == self.array[8] {
+            return true;
+        } else if self.array[0] == self.array[3] && self.array[3] == self.array[6] {
+            return true;
+        } else if self.array[1] == self.array[4] && self.array[4] == self.array[7] {
+            return true;
+        } else if self.array[2] == self.array[5] && self.array[5] == self.array[8] {
+            return true;
+        } else if self.array[0] == self.array[4] && self.array[4] == self.array[8] {
+            return true;
+        } else if self.array[2] == self.array[4] && self.array[4] == self.array[6] {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Prompts user to enter board position and handles user input
+    fn get_player_entry(&self, player_number: &str) -> Result<usize, EntryError> {
+        // Print player prompts and board
+        println!(
+        "Player {}, please enter a number, 0 to 8, to pick a spot for your play. Enter q to quit game.",
+        player_number
+    );
+        self.print_board();
+
+        let mut chosen_index = String::new();
+
+        // Prompt user for index input
+        io::stdin()
+            .read_line(&mut chosen_index)
+            .expect("Failed to read line");
+
+        // Convert user input to usize to access board array
+        match chosen_index.trim().parse() {
+            Ok(num) if num <= 8 => Ok(num),
+            // User entered number greater than 8
+            Ok(_) => Err(EntryError::InvalidIndex),
+            // User entered `q` character to quit game
+            Err(_) if chosen_index.trim() == "q" => Err(EntryError::UserQuit),
+            // User entered none int character
+            Err(_) => Err(EntryError::InvalidCharacter),
+        }
+    }
+
+    fn set_character(&mut self, index: usize, character: &'static str) {
+        self.array[index] = character;
+    }
+}
+
 fn main() {
     // Create board array of numbers for printing board
-    let mut board_array: [&'static str; 9] = setup_game();
+    let mut board: Board = setup_game();
 
     let mut is_player_one = true;
     let mut player_character;
@@ -21,14 +103,14 @@ fn main() {
         loop {
             // Get user input to set player board entry
             // break loop if user chooses to quit and don't set value for player entry
-            player_entry = match get_player_entry(&player_number, &board_array) {
+            player_entry = match board.get_player_entry(&player_number) {
                 Ok(index) => Some(index),
                 Err(error) => {
                     match error {
                         EntryError::UserQuit => {
                             println!("You have chosen to end the game. See you next round!");
                             break;
-                        },
+                        }
                         EntryError::InvalidCharacter => println!(
                             "You did not enter a number. Please enter a number between 0 and 8."
                         ),
@@ -46,8 +128,8 @@ fn main() {
         match player_entry {
             // Replace number in array with player character that user entered, X or O
             Some(value) => {
-                board_array[value] = player_character;
-                print_board(&board_array);
+                board.set_character(value, player_character);
+                board.print_board();
             }
             // if player_entry never initialized, this means user chose to quit
             // break the loop and end the game
@@ -55,7 +137,7 @@ fn main() {
         }
 
         // Check for winner
-        let is_winner = check_for_win(&board_array);
+        let is_winner = board.check_for_win();
 
         // Get game state based on winner status and print win status for user
         let game_state = handle_win_result(is_winner, player_number, turn_count);
@@ -83,47 +165,21 @@ fn player_information(is_player_one: bool) -> (&'static str, &'static str) {
 }
 
 // Create board array of numbers for printing board and print welcome message for user
-fn setup_game() -> [&'static str; 9] {
+fn setup_game() -> Board {
     println!("Welcome to the tic tac toe!");
-    ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    Board {
+        array: ["0", "1", "2", "3", "4", "5", "6", "7", "8"],
+    }
 }
 
 // Error returned if user enters invalid character when prompted for board index input
 // InvalidIndex: Number greater than 8 entered, larger than board size
 // InvalidCharacter: None usize convertible character entered
+#[derive(Debug)]
 enum EntryError {
     InvalidIndex,
     InvalidCharacter,
     UserQuit,
-}
-
-// Prompts user to enter board position and handles user input
-fn get_player_entry(player_number: &str, board_array: &[&str]) -> Result<usize, EntryError> {
-    // Print player prompts and board
-    println!(
-        "Player {}, please enter a number, 0 to 8, to pick a spot for your play. Enter q to quit game.",
-        player_number
-    );
-    print_board(&board_array);
-
-    let mut chosen_index = String::new();
-
-    // Prompt user for index input
-    io::stdin()
-        .read_line(&mut chosen_index)
-        .expect("Failed to read line");
-
-    // User entered `q` character to quit game
-    if chosen_index.trim() == "q" {
-        return Err(EntryError::UserQuit)
-    }
-
-    // Convert user input to usize to access board array
-    match chosen_index.trim().parse() {
-        Ok(num) if num <= 8 => Ok(num),
-        Ok(_) => Err(EntryError::InvalidIndex),
-        Err(_) => Err(EntryError::InvalidCharacter),
-    }
 }
 
 // Enum for state of game
@@ -145,49 +201,4 @@ fn handle_win_result(is_winner: bool, player_number: &str, turn_count: i32) -> G
     } else {
         GameState::Continue
     }
-}
-
-// Compare characters by indexes on board to verify if there is currently a winner
-fn check_for_win(board_array: &[&str]) -> bool {
-    if board_array[0] == board_array[1] && board_array[1] == board_array[2] {
-        return true;
-    } else if board_array[3] == board_array[4] && board_array[4] == board_array[5] {
-        return true;
-    } else if board_array[6] == board_array[7] && board_array[7] == board_array[8] {
-        return true;
-    } else if board_array[0] == board_array[3] && board_array[3] == board_array[6] {
-        return true;
-    } else if board_array[1] == board_array[4] && board_array[4] == board_array[7] {
-        return true;
-    } else if board_array[2] == board_array[5] && board_array[5] == board_array[8] {
-        return true;
-    } else if board_array[0] == board_array[4] && board_array[4] == board_array[8] {
-        return true;
-    } else if board_array[2] == board_array[4] && board_array[4] == board_array[6] {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-fn print_board(board_array: &[&str]) {
-    // Board should print as:
-    // |0|1|2|
-    // |3|4|5|
-    // |6|7|8|
-    println!(
-        "
-    |{}|{}|{}|
-    |{}|{}|{}|
-    |{}|{}|{}|",
-        board_array[0],
-        board_array[1],
-        board_array[2],
-        board_array[3],
-        board_array[4],
-        board_array[5],
-        board_array[6],
-        board_array[7],
-        board_array[8]
-    );
 }
